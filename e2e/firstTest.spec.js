5f7b5b88b67bd865e07340a20fc6095eef236296
Waneed Parkash <waneed@plerosys.com>
2019-03-05 14:36:31 +0500

    init

---
+++ e2e/firstTest.spec.js
@@ -0,0 +1,11 @@
+import Comp from '../src/comp';
+
+describe('Example', () => {
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+
+  it('should have welcome screen', async () => {
+    await expect(element(by.id('touch-me'))).toBeVisible();
+  });
+});
---
+++ src/comp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+class Comp extends React.Component {
+    render () {
+        return (
+            <View>
+                <Text>Hello</Text>
+            </View>
+        );
+    }
+}
+
+export default Comp;
